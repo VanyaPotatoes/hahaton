77d50a2743d91ff7db871c2b61107701bfdaeb72
VanyaPotatoes <96769299+VanyaPotatoes@users.noreply.github.com>
2022-11-13 09:52:29 +0300

    Update ML.py

--- ML.py
+++ ML.py
@@ -5,25 +5,17 @@ import seaborn as sns
 from unittest import skip
 import numpy as np
 import pandas as pd 
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 
-train_data = pd.read_csv("C:\\Users\\Администратор\\Desktop\\ready.csv")
-sns.histplot(data=train_data["region"])
-#Output plot on Jupiter Notebook
-train_data = train_data.drop("Unnamed: 0", axis=1)
-train_data.dropna(subset="region")
-df= train_data.drop(index=train_data.index[1:3653500])
-df.shape
-#(507, 6)
+train_data = pd.read_csv("C:\\Users\\Администратор\\Desktop\\okved1.csv")
 km=KMeans(n_clusters=5)
-y_predicted = km.fit_predict(df[['okved','region']])
-y_predicted
-km.cluster_centers_
-""" 
-array([[46.01324503, 47.09271523],
-       [45.53157895, 72.6       ],
-       [80.5106383 , 67.31914894],
-       [43.23333333, 16.62222222],
-       [86.03448276, 26.03448276]])
-"""
-df['Clusters'] = km.labels_
-sns.scatterplot(x="okved", y="region",hue = 'Clusters',  data=df,palette='viridis')
+y_predicted = km.fit_predict(train_data[['okved','registration_date']])
+scaler = MinMaxScaler()
+scale = scaler.fit_transform(train_data[['okved','registration_date']])
+train_data = pd.DataFrame(scale, columns = ['okved','registration_date']);
+train_data['Clusters'] = km.labels_
+sns.scatterplot(x="registration_date", y="okved",hue = 'Clusters',  data=train_data, alpha=0.05)
+
+train_data.to_csv("C:\\Users\\Администратор\\Desktop\\predicted_data.csv")
+#output the plot
